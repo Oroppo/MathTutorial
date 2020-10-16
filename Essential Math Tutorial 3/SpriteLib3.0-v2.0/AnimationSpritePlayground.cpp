26f55cc4c1dc7773df43ef60c78a312e443c7371
Gabe Mercier <gabemercier@live.ca>
2020-10-16 01:00:59 -0400

    Changed the Animation Sprite Playground

--- Essential Math Tutorial 3/SpriteLib3.0-v2.0/AnimationSpritePlayground.cpp
+++ Essential Math Tutorial 3/SpriteLib3.0-v2.0/AnimationSpritePlayground.cpp
@@ -2,60 +2,98 @@
 #include "Utilities.h"
 
 AnimationSpritePlayground::AnimationSpritePlayground(std::string name)
-: Scene(name)
+	: Scene(name)
 {
-	m_gravity = b2Vec2(0.f, 0.f);
+	//No gravity this is a top down scene
+	m_gravity = b2Vec2(0.f, -1000.f);
 	m_physicsWorld->SetGravity(m_gravity);
+
 }
 
 void AnimationSpritePlayground::InitScene(float windowWidth, float windowHeight)
 {
+	//Dynamically alloctes the register
 	m_sceneReg = new entt::registry;
 
 	ECS::AttachRegister(m_sceneReg);
 
 	float aspectRatio = windowWidth / windowHeight;
-	//Setup MainCamera Entity
+
+
+	//Setup main camera entity
 	{
-		//Creates Camera entity
+		//Creates camera
 		auto entity = ECS::CreateEntity();
 		ECS::SetIsMainCamera(entity, true);
 
-		//Creates new orthographic camera
+		//Creates new otrhographic camera
 		ECS::AttachComponent<Camera>(entity);
 		ECS::AttachComponent<HorizontalScroll>(entity);
-		ECS::AttachComponent <VerticalScroll>(entity);
+		ECS::AttachComponent<VerticalScroll>(entity);
 
 		vec4 temp = vec4(-240.f, 240.f, -240.f, 240.f);
 		ECS::GetComponent<Camera>(entity).SetOrthoSize(temp);
 		ECS::GetComponent<Camera>(entity).SetWindowSize(vec2(float(windowWidth), float(windowHeight)));
 		ECS::GetComponent<Camera>(entity).Orthographic(aspectRatio, temp.x, temp.y, temp.z, temp.w, -100.f, 100.f);
 
-
-		//Attaches the camera to vertical and horizontal scrolls
 		ECS::GetComponent<HorizontalScroll>(entity).SetCam(&ECS::GetComponent<Camera>(entity));
 		ECS::GetComponent<VerticalScroll>(entity).SetCam(&ECS::GetComponent<Camera>(entity));
 
 	}
 
-	//Ghost entity
-	{
+	//Setup Floor
+	for (float i = -508; i <= 127; i = i + 127) {
+
+		//Creates entity
 		auto entity = ECS::CreateEntity();
-		ECS::SetIsMainPlayer(entity, true);
 
 		//Add components
+		ECS::AttachComponent<Sprite>(entity);
+		ECS::AttachComponent<Transform>(entity);
+		ECS::AttachComponent<PhysicsBody>(entity);
+
+		//Sets up components
+		std::string fileName = "Floor.png";
+		ECS::GetComponent<Sprite>(entity).LoadSprite(fileName, 127, 50);
+		ECS::GetComponent<Transform>(entity).SetPosition(vec3(0.f, 0.f, 1.f));
+
+		auto& tempSpr = ECS::GetComponent<Sprite>(entity);
+		auto& tempPhsBody = ECS::GetComponent<PhysicsBody>(entity);
+
+		float shrinkX = 0.f;
+		float shrinkY = 0.f;
+
+		b2Body* tempBody;
+		b2BodyDef tempDef;
+		tempDef.type = b2_staticBody;
+		tempDef.position.Set(float32(i), float32(-200.f));
+
+		tempBody = m_physicsWorld->CreateBody(&tempDef);
+
+		tempPhsBody = PhysicsBody(tempBody, float(tempSpr.GetWidth() - shrinkX), float(tempSpr.GetHeight() - shrinkY), vec2(0.f, 0.f), false);
+
+	}
+
+
+
+	//Setup Link
+	{
+		auto entity = ECS::CreateEntity();
+
+		ECS::SetIsMainPlayer(entity, true);
 		ECS::AttachComponent<Player>(entity);
 		ECS::AttachComponent<Sprite>(entity);
 		ECS::AttachComponent<Transform>(entity);
 		ECS::AttachComponent<AnimationController>(entity);
+		ECS::AttachComponent<PhysicsBody>(entity);
 
-		//Sets up components
-		std::string fileName = "Boo8bit.png";
-		std::string animations = "LinkSprites.json";
-		
-		ECS::GetComponent<Sprite>(entity).LoadSprite(fileName, 50, 50);
-		ECS::GetComponent<Sprite>(entity).SetTransparency(1.f);
-		ECS::GetComponent<Transform>(entity).SetPosition(vec3(0.f, 50.f, 1.f));
+		//Set up components
+		std::string fileName = "spritesheets/Link.png";
+		std::string animations = "linkAnimations.json";
+		ECS::GetComponent<Player>(entity).InitPlayer(fileName, animations, 20, 30,
+			&ECS::GetComponent<Sprite>(entity),
+			&ECS::GetComponent<AnimationController>(entity),
+			&ECS::GetComponent<Transform>(entity), false);
 
 		auto& tempSpr = ECS::GetComponent<Sprite>(entity);
 		auto& tempPhsBody = ECS::GetComponent<PhysicsBody>(entity);
@@ -66,25 +104,28 @@ void AnimationSpritePlayground::InitScene(float windowWidth, float windowHeight)
 		b2Body* tempBody;
 		b2BodyDef tempDef;
 		tempDef.type = b2_dynamicBody;
-		tempDef.position.Set(float32(0.f), float32(50.f));
+		tempDef.position.Set(float32(0.f), float32(0.f));
 
 		tempBody = m_physicsWorld->CreateBody(&tempDef);
 
-		tempPhsBody = PhysicsBody(tempBody, float(tempSpr.GetWidth()), float(tempSpr.GetHeight()), vec2(0.f, 0.f), false);
+		tempPhsBody = PhysicsBody(tempBody, float(tempSpr.GetWidth() - shrinkX), float(tempSpr.GetHeight() - shrinkY), vec2(0.f, 0.f), false);
 
 		ECS::GetComponent<PhysicsBody>(MainEntities::MainPlayer()).GetBody()->SetFixedRotation(true);
-
 	}
+
+	ECS::GetComponent<HorizontalScroll>(MainEntities::MainCamera()).SetFocus(&ECS::GetComponent<Transform>(MainEntities::MainPlayer()));
+	ECS::GetComponent<VerticalScroll>(MainEntities::MainCamera()).SetFocus(&ECS::GetComponent<Transform>(MainEntities::MainPlayer()));
 }
 
 void AnimationSpritePlayground::Update()
 {
+	auto& player = ECS::GetComponent<Player>(MainEntities::MainPlayer());
 	Scene::AdjustScrollOffset();
+	player.Update();
 }
 
 void AnimationSpritePlayground::KeyboardHold()
 {
-
 }
 
 void AnimationSpritePlayground::KeyboardDown()
