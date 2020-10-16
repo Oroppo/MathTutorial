87107e9d3d9e8de0dc20a56c11c526d49265b560
Dante Arruda <dante.arruda416@gmail.com>
2020-10-16 06:03:13 -0400

    Added Endpoint and Death Condition

--- Essential Math Tutorial 3/SpriteLib3.0-v2.0/AnimationSpritePlayground.cpp
+++ Essential Math Tutorial 3/SpriteLib3.0-v2.0/AnimationSpritePlayground.cpp
@@ -5,7 +5,7 @@ AnimationSpritePlayground::AnimationSpritePlayground(std::string name)
 	: Scene(name)
 {
 	//No gravity this is a top down scene
-	m_gravity = b2Vec2(0.f, -175.f);
+	m_gravity = b2Vec2(0.f, -200.f);
 	m_physicsWorld->SetGravity(m_gravity);
 
 }
@@ -31,7 +31,7 @@ void AnimationSpritePlayground::InitScene(float windowWidth, float windowHeight)
 		ECS::AttachComponent<HorizontalScroll>(entity);
 		ECS::AttachComponent<VerticalScroll>(entity);
 
-		vec4 temp = vec4(-240.f, 240.f, -240.f, 240.f);
+		vec4 temp = vec4(-220.f, 220.f, -220.f, 220.f);
 		ECS::GetComponent<Camera>(entity).SetOrthoSize(temp);
 		ECS::GetComponent<Camera>(entity).SetWindowSize(vec2(float(windowWidth), float(windowHeight)));
 		ECS::GetComponent<Camera>(entity).Orthographic(aspectRatio, temp.x, temp.y, temp.z, temp.w, -100.f, 100.f);
@@ -42,7 +42,7 @@ void AnimationSpritePlayground::InitScene(float windowWidth, float windowHeight)
 	}
 
 	//Setup Floor
-	for (float i = -508; i <= 127; i = i + 127) {
+	for (float i = -508; i <= 889; i = i + 127) {
 
 		//Creates entity
 		auto entity = ECS::CreateEntity();
@@ -54,7 +54,7 @@ void AnimationSpritePlayground::InitScene(float windowWidth, float windowHeight)
 
 		//Sets up components
 		std::string fileName = "Floor.png";
-		ECS::GetComponent<Sprite>(entity).LoadSprite(fileName, 127, 50);
+		ECS::GetComponent<Sprite>(entity).LoadSprite(fileName, 130, 50);
 		ECS::GetComponent<Transform>(entity).SetPosition(vec3(0.f, 0.f, 1.f));
 
 		auto& tempSpr = ECS::GetComponent<Sprite>(entity);
@@ -73,8 +73,103 @@ void AnimationSpritePlayground::InitScene(float windowWidth, float windowHeight)
 		tempPhsBody = PhysicsBody(tempBody, float(tempSpr.GetWidth() - shrinkX), float(tempSpr.GetHeight() - shrinkY), vec2(0.f, 0.f), false);
 
 	}
+	{
+		//Creates EndFlag
+		auto entity = ECS::CreateEntity();
 
-	
+		//Add components
+		ECS::AttachComponent<Sprite>(entity);
+		ECS::AttachComponent<Transform>(entity);
+		ECS::AttachComponent<PhysicsBody>(entity);
+
+		//Sets up components
+		std::string fileName = "EndFlag.png";
+		ECS::GetComponent<Sprite>(entity).LoadSprite(fileName, 100, 400);
+		ECS::GetComponent<Transform>(entity).SetPosition(vec3(0.f, 0.f, 3.f));
+
+		auto& tempSpr = ECS::GetComponent<Sprite>(entity);
+		auto& tempPhsBody = ECS::GetComponent<PhysicsBody>(entity);
+
+		float shrinkX = 0.f;
+		float shrinkY = 0.f;
+
+		b2Body* tempBody;
+		b2BodyDef tempDef;
+		tempDef.type = b2_staticBody;
+		tempDef.position.Set(float32(350.f), float32(0.f));
+
+		tempBody = m_physicsWorld->CreateBody(&tempDef);
+
+		tempPhsBody = PhysicsBody(tempBody, float(tempSpr.GetWidth() - shrinkX), float(tempSpr.GetHeight() - shrinkY), vec2(10000.f, 0.f), false);
+	}
+	//Setup BasicBlock
+	int j = -200;
+		for (float i = -508; i <= 889; i = i + 127) {
+
+			//Creates entity
+			auto entity = ECS::CreateEntity();
+
+			//Add components
+			ECS::AttachComponent<Sprite>(entity);
+			ECS::AttachComponent<Transform>(entity);
+			ECS::AttachComponent<PhysicsBody>(entity);
+
+			//Sets up components
+			std::string fileName = "BasicBlock.png";
+			ECS::GetComponent<Sprite>(entity).LoadSprite(fileName, 50, 50);
+			ECS::GetComponent<Transform>(entity).SetPosition(vec3(0.f, 0.f, 2.f));
+
+			auto& tempSpr = ECS::GetComponent<Sprite>(entity);
+			auto& tempPhsBody = ECS::GetComponent<PhysicsBody>(entity);
+
+			float shrinkX = 0.f;
+			float shrinkY = 0.f;
+
+			b2Body* tempBody;
+			b2BodyDef tempDef;
+			tempDef.type = b2_staticBody;
+			tempDef.position.Set(float32(i), float32(j));
+
+			tempBody = m_physicsWorld->CreateBody(&tempDef);
+
+			tempPhsBody = PhysicsBody(tempBody, float(tempSpr.GetWidth() - shrinkX), float(tempSpr.GetHeight() - shrinkY), vec2(0.f, 0.f), false);
+
+			j=j+20;
+	}
+	//Setup LavaBad
+	for (float i = -444.5; i <= 889; i = i + 63.5) {
+		
+
+		//Creates entity
+		auto entity = ECS::CreateEntity();
+
+		//Add components
+		ECS::AttachComponent<Sprite>(entity);
+		ECS::AttachComponent<Transform>(entity);
+		ECS::AttachComponent<PhysicsBody>(entity);
+
+		//Sets up components
+		std::string fileName = "LavaBad.png";
+		ECS::GetComponent<Sprite>(entity).LoadSprite(fileName, 100, 35);
+		ECS::GetComponent<Transform>(entity).SetPosition(vec3(0.f, 0.f, 0.f));
+
+		auto& tempSpr = ECS::GetComponent<Sprite>(entity);
+		auto& tempPhsBody = ECS::GetComponent<PhysicsBody>(entity);
+
+		float shrinkX = 0.f;
+		float shrinkY = 0.f;
+
+		b2Body* tempBody;
+		b2BodyDef tempDef;
+		tempDef.type = b2_staticBody;
+		tempDef.position.Set(float32(i), float32((-175.f)));
+
+		tempBody = m_physicsWorld->CreateBody(&tempDef);
+
+		tempPhsBody = PhysicsBody(tempBody, float(tempSpr.GetWidth() - shrinkX), float(tempSpr.GetHeight() - shrinkY), vec2(0.f, 0.f), false);
+		
+
+	}
 
 	//Setup Link
 	{
@@ -107,39 +202,35 @@ void AnimationSpritePlayground::InitScene(float windowWidth, float windowHeight)
 		tempDef.position.Set(float32(0.f), float32(30.f));
 
 
-
-
-
 		tempBody = m_physicsWorld->CreateBody(&tempDef);
 
 		tempPhsBody = PhysicsBody(tempBody, float(tempSpr.GetWidth() - shrinkX), float(tempSpr.GetHeight() - shrinkY), vec2(0.f, 0.f), false);
-		tempPhsBody.GetBody()->GetFixtureList()->SetFriction(0);
 
 		ECS::GetComponent<PhysicsBody>(MainEntities::MainPlayer()).GetBody()->SetFixedRotation(true);
-	}
 
-	
+
+	}
 
 	ECS::GetComponent<HorizontalScroll>(MainEntities::MainCamera()).SetFocus(&ECS::GetComponent<Transform>(MainEntities::MainPlayer()));
 	ECS::GetComponent<VerticalScroll>(MainEntities::MainCamera()).SetFocus(&ECS::GetComponent<Transform>(MainEntities::MainPlayer()));
 }
 
+
 void AnimationSpritePlayground::Update()
 {
 	auto& player = ECS::GetComponent<Player>(MainEntities::MainPlayer());
 	Scene::AdjustScrollOffset();
-	vec3 vel = vec3(0.f, 0.f, 0.f);
 	player.Update();
 	
 	unsigned int  position = ECS::GetComponent<Transform>(MainEntities::MainPlayer()).GetPositionX();
 	std::cout << position << "\n";
-	if (position >= 400 && position <= 600) {
+	if (position >= 400 && position<=600) {
 		std::cout << "--------------------You Won!--------------------";
 
 		exit(0);
 	}
-
 }
+
 
 void AnimationSpritePlayground::KeyboardHold()
 {
